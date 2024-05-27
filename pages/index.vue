ae326d0003c2e820068b7491c5c066316d0a982e
tito <chitose0720@icloud.com>
2024-05-27 18:45:07 +0900

    indextest

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-spacer />
-    <div class="flex justify-center items-center">
+    <div class="flex justify-center items-center  bg-blue-500 text-3xl">
       <p>TODOリスト</p>
     </div>
 
@@ -9,13 +9,13 @@
       <input 
         v-model="newTodo" 
         required
-        placeholder="input new todo "
-        class="w-11/12"
+        placeholder="input new todo" 
+        class="w-5/6 m-3 border-2 "
       />
       <TodoButton title="add" :click-action="addTodo"></TodoButton>
     </form>
 
-    <div class="list-disc">
+    <div class="list-disc m-3">
       <li v-for="todo in todos" :key="todo.id">
         {{ todo.text }}
         <TodoButton title="delete" :click-action="removeTodo" :todo="todo" />
