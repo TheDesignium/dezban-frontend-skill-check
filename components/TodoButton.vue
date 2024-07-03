e81845bbf06a9e26c2c4e370d0029a1bd75fd1d6
tito <chitose0720@icloud.com>
2024-07-04 00:00:15 +0900

    TodoButton.vue

--- components/TodoButton.vue
+++ components/TodoButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="text-red-700  border-4  border-red-500 " @click="clickAction(todo)">
+  <button class=" text-red-700  border-4  border-blue-300 rounded bg-blue-300 " @click="clickAction(todo)">
     {{ title }}
   </button>
 </template>
