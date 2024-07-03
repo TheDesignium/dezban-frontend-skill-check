359bf7ca990cfc57805a3a6e0e204aa62610412d
tito <chitose0720@icloud.com>
2024-07-03 22:52:40 +0900

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
-<template>
-  <div>
+<template >
+
+   <div>
+    
     <v-spacer />
     <div class="flex justify-center items-center  bg-blue-500 text-3xl">
-      <p>TODOリスト</p>
+    <p class ="my-2 "> TODOリスト</p>
     </div>
-
+    <div class="bg-hero h-screen">
     <form @submit.prevent="addTodo">
       <input 
         v-model="newTodo" 
         required
-        placeholder="input new todo" 
-        class="w-5/6 m-3 border-b-2  border-4 border-green-500"
+        placeholder=" input new todo" 
+        class="w-5/6 ml-8 mt-5 border-b-2  border-4 border-green-500 rounded
+        "
       />
       <TodoButton title="add" :click-action="addTodo"></TodoButton>
     </form>
 
-    <div class="list-disc m-3">
+    <div class="list-disc ml-10 mt-4 text-red-400">
       <li v-for="todo in todos" :key="todo.id">
         {{ todo.text }}
         <TodoButton title="delete" :click-action="removeTodo" :todo="todo" />
       </li>
     </div>
   </div>
+  </div>
+
 </template>
 
 <script>
@@ -53,3 +58,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.body-bg{
+  background-image:url('');
+    background-repeat:  no-repeat;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
