2c105864fb8b292dad03d4bf724a718d1b7c539f
tito <chitose0720@icloud.com>
2024-07-16 12:16:19 +0900

    tailwind2.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 module.exports = {
-      content: [
-        "./components/**/*.{js,vue,ts}",
-        "./layouts/**/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
-        "./app.vue",
-        "./error.vue",
-      ],
-      theme: {
-        extend: {
-        backgroundImage: {
-          'hero': "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676')",
-        },
-        colors: {
-          'regal-blue': '#243C5A',
-          'plum': '#DDA0DD '
-        },
-        },
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676')",
       },
-      plugins: [],
-      
-
+      colors: {
+        'regal-blue': '#243C5A',
+        plum: '#DDA0DD ',
+      },
+    },
+  },
+  plugins: [],
 }
