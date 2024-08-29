cee3d28c1e549080eef313b03843d8152ff2e05d
airi <airi151108@icloud.com>
2024-08-30 08:09:20 +0900

    first commit

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,7 +12,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_c1c57276 from 'nuxt_plugin_plugin_c1c57276' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_plugin_d762bd46 from 'nuxt_plugin_plugin_d762bd46' // Source: .\\components\\plugin.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -178,8 +178,8 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_c1c57276 === 'function') {
-    await nuxt_plugin_plugin_c1c57276(app.context, inject)
+  if (typeof nuxt_plugin_plugin_d762bd46 === 'function') {
+    await nuxt_plugin_plugin_d762bd46(app.context, inject)
   }
 
   // Lock enablePreview in context
--- components/TodoButton.vue
+++ components/TodoButton.vue
@@ -1,8 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <button class="text-700 border-2 border-500" @click="clickAction(todo)">
+      {{ title }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    clickAction: {
+      type: Function,
+    },
+    todo: {
+      type: String,
+      default: '',
+    },
+  },
+})
 </script>
